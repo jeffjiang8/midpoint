8c59eff8fa7c379a308d598ff97a85e28e182d95
Jeff Jiang <jeffjiang8@gmail.com>
2020-06-17 17:28:26 -0400

    step5

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ function midpoint(list) {
     let slow = list.getFirst() // or list.head
     let fast = list.getFirst()
 
-    
+    while(fast.next && fast.next.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    return slow // when fast.next.next = null, the function
+                // will exit the while loop; because fast is
+                // moving twice as fast as slow, the node that
+                // slow is pointing at has to be the midpoint
 }
